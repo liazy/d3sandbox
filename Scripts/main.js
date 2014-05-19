4ab32cbc4973b09889ae89d209ce8286dcc29783
liazy <l.p.mitchell8@gmail.com>
2014-05-19 16:28:32 +0100

    Basic bar graph. Transition is not yet working as desired.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,22 +1,80 @@
-var dataset = [ 5, 10, 15, 20, 25 ];
+$(document).ready(function() {
+	var dataset = [];
 
-var context = d3.select("#bars")
-	.selectAll("div")
-	// bind data
-    .data(dataset);
 	
- // add things that aren't there
- context.enter()	
-    .append("div")
-	.attr("class", function(d) {
-		var output = 'databar';
+	
+	function bind() {
+		var max = 100;
+		var seriesWidth = 100 / dataset.length;
+
+		// bind data
+		var context = d3.select("#bars")
+						.selectAll("div")
+						.data(dataset);
+			
+		 // add things that aren't there
+		 context.enter()	
+			.append("div")
+			// set initial height for transitioning...
+			.style("height", function(d) {
+				return '100%';
+			})
+			// set default class...
+			.attr("class", function(d) {
+				return 'databar';
+			});
 		
-		if (d > 15) {   //Threshold of 15
-			output += ' databar-error';
+		// remove things that shouldn't be there
+		context.exit()	
+			   .remove();
+		
+		// update new and old...
+		context.attr("class", function(d) {
+				var output = 'databar';
+				
+				if (d.value < 30) {   //Threshold of 15
+					output += ' databar-error';
+				}
+				
+				return output;
+			})
+			.transition()
+			.style("width", function(d) {
+				return seriesWidth + '%';
+			})
+			.style("left", function(d, index) {
+				return seriesWidth*index + '%';
+			})
+			.style("height", function(d) {
+				return (d.value*100)/max + '%';
+			});
+	}
+
+	function add() {
+		var next = Math.round(Math.random()*100);
+		dataset.push({ value: next});
+	};
+	
+	add();	
+	add();	
+	add();	
+	add();	
+	add();	
+	bind();
+	
+	$('[data-action="swap"]').on('click', function() {
+		for (var i = 2; i < dataset.length; i++) {
+			var swap = dataset[i];
+			dataset[i] = dataset[i-2];
+			dataset[i-2] = swap;
 		}
 		
-		return output;
-	})
-	.style("height", function(d) {
-		return d + 'px';
+		bind();
 	});
+	
+	$('[data-action="add"]').on('click', function() {
+		add();		
+		bind();
+	});
+});
+	
