4ce2cd4ab821cd3cad8aceac09c6982d13ac362b
liazy <l.p.mitchell8@gmail.com>
2014-05-20 08:55:46 +0100

    Nice animation.

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
-	var dataset = [];
-
-	
+	var dataset = [];	
 	
 	function bind() {
+		var data = [];
+		
+		for (var i = 0; i < dataset.length; i++) {
+			data[i] = dataset[i];
+		}
+		
+		// add an extra 0 value bar to animate from when we add a new node...
+		data.push({value: 0, index: dataset.length});
+	
 		var max = 100;
 		var seriesWidth = 100 / dataset.length;
+		
+		var margin = seriesWidth/(dataset.length*2)
 
 		// bind data
 		var context = d3.select("#bars")
 						.selectAll("div")
-						.data(dataset);
+						.data(data);
 			
 		 // add things that aren't there
 		 context.enter()	
@@ -24,6 +33,11 @@ $(document).ready(function() {
 		context.exit()	
 			   .remove();
 		
+		// get a new context to separate enter from update animations...
+		var context = d3.select("#bars")
+						.selectAll("div")
+						.data(dataset);
+		
 		// update new and old...
 		context.attr("class", function(d) {
 				var output = 'databar';
@@ -35,19 +49,25 @@ $(document).ready(function() {
 				return output;
 			})
 			.style("width", function(d) {
-				return seriesWidth + '%';
+				// margin is added to each side of the bar for prettiness...
+				return (seriesWidth - 2 * margin) + '%';
 			})
-			.style("left", function(d, index) {
-				return seriesWidth*index + '%';
+			.style("left", function(d) {
+				// the elements are always linked to the item at the same index in the array
+				// keeping track of the index ourselves allows us to do the side to side animation...
+				return (seriesWidth * d.index + margin) + '%';
 			})
 			.style("height", function(d) {
 				return (d.value*100)/max + '%';
+			})
+			.text(function(d) {
+				return d.id;
 			});
 	}
 
 	function add() {
 		var next = Math.round(Math.random()*100);
-		dataset.push({ value: next});
+		dataset.push({ value: next, index: dataset.length});
 	};
 	
 	add();	
@@ -57,11 +77,21 @@ $(document).ready(function() {
 	add();	
 	bind();
 	
+	$('[data-action="move"]').on('click', function() {
+		for (var i = 2; i < dataset.length; i+=2) {
+			var swap = dataset[i].index;
+			dataset[i].index = dataset[i-2].index;
+			dataset[i-2].index = swap;
+		}
+		
+		bind();
+	});
+	
 	$('[data-action="swap"]').on('click', function() {
-		for (var i = 2; i < dataset.length; i++) {
-			var swap = dataset[i];
-			dataset[i] = dataset[i-2];
-			dataset[i-2] = swap;
+		for (var i = 2; i < dataset.length; i+=2) {
+			var swap = dataset[i].value;
+			dataset[i].value = dataset[i-2].value;
+			dataset[i-2].value = swap;
 		}
 		
 		bind();
